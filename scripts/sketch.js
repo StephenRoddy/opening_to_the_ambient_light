164a247eea862eb1f8d8e2045775b13787825dba
Stephen Roddy <roddyst@tcd.ie>
2022-01-11 15:54:40 +0000

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -32,36 +32,36 @@ audio = loadSound("../audio/Opening to the Ambient Light.mp3");
 
 function setup() {
   cnv = createCanvas(1000, 1000, WEBGL);
-  
+
   noStroke();
   fill(255, 255, 255);
-  
-  
+
+
   fft = new p5.FFT();
   fft.setInput(audio);
   cnv.mouseClicked(togglePlay);
-  
+
   lpX3 = random(-width/2,width/2);
   lpY3 = random(-height/2,height/2);
-  
+
   lpX2 = random(-width/2,width/2);
   lpY2 = random(-height/2,height/2);
-  
-  
+
+
   // crawlers start in different quadrants
   lpX = random(width/2);
   lpY = random(-height/2);
-  
+
   lpX4 = random(-width/2);
   lpY4 = random(height/2);
-  
+
 }
 
 
 function draw() {
   translate(-width/2,-height/2,0); // Translate for the WEBGL coordinat system. Moving elements to top left nad corner of canvas.
-  background(0); 
-  
+  background(0);
+  blendMode(REPLACE);
   fft.setInput(audio);
   let waveform = fft.waveform(); // amplitudes along time domain
   let spectrum = fft.analyze();
@@ -71,61 +71,64 @@ function draw() {
   mid = fft.getEnergy("mid");
   highMid = fft.getEnergy("highMid");
   treble = fft.getEnergy("treble");
-  
+
   /* Mappings*/
   amp_cos = map(bass+lowMid,0,510,0,30);
   amp_sin = map(mid+treble+highMid,0,765,0,30);
-  
+
   cos_pos  = amp_cos;
   sin_pos = amp_sin;
-  
-  particle_size = map(bass+lowMid,0,510,0,7); //original was a size of 1 to 7
+
+  ambientMaterial(bass);  // controling the materials and shininess from the data
+  shininess((treble+highMid+mid)/3);
+
+  particle_size = map(bass+lowMid,0,510,0,7); //original was a size of 0 to 7
   fill(treble,(lowMid+mid+highMid)/3,bass); // color map
 
  // ambientLight(mid);
   // make an x and y grid of ellipses
   for (let x = 0; x <= width; x = x + 20) {
-        
-    
+
+
     /* Call Lights functions */
     light_1(bass);
     light_2((treble+highMid+mid)/3);
     light_3((lowMid+mid+highMid)/3);
     light_4((bass+lowMid)/2);
-    
 
 
-        xPos = width/2 + cos(cos_theta) * amp_cos; 
-    
+
+        xPos = width/2 + cos(cos_theta) * amp_cos;
+
     for (let y = 0; y <= height; y = y + 20) {
       // set up the yPosition along a sin wave
-      
+
         cos_theta +=  map(treble+highMid,0,510,.4999,.5); //different theta values for X and Y as we update theta before writing Y poxitions
 
         sin_theta +=  map(bass+lowMid+mid,0,765,.4999,.5); //different theta values for X and Y as we update theta before writing Y poxitions
 
 
-      
-       yPos = height/2 + sin(sin_theta) * amp_sin; 
-      
+
+       yPos = height/2 + sin(sin_theta) * amp_sin;
+
       const xAngle = map(xPos, 0, width, - cos_pos * PI, sin_pos * PI );
       const yAngle = map(yPos, 0, height,- sin_pos * PI, cos_pos * PI );
       // and also varies based on the particle's location
       const angle = xAngle * (x / width) + yAngle * (y / height);
-      
+
       // each particle moves up and down along the y axis and sinde to side on the x
       const myX = x + 30 * cos(2 * PI  + angle); // no x direction movement
       const myY = y + 20 * sin(1 * PI  + angle);
-      
+
       ellipse(myX, myY, particle_size); // draw particle
-    
+
     }
-  
+
   }
-  
+
   t = t + 0.01; // update time
-  
-  
+
+
 }
 
 
@@ -189,7 +192,7 @@ function light_4(lFreq){ // crawler again with new lpX -- need to fix theese and
 
     /* Light Processing BEfore breaking into functions*/
    /*
-  //Light 1    
+  //Light 1
     if(lpX > width/2  || lpY > height/2 || lpX <= -width/2 || lpY <= -height/2){
       lpX = random(width/2);
       lpY = random(height/2);
@@ -200,9 +203,9 @@ function light_4(lFreq){ // crawler again with new lpX -- need to fix theese and
   //  let pl1=  pointLight(250, 250, 250, lpX, lpY, 10+((bass/255)*10));
     let pl1=  pointLight(bass, bass, bass, lpX, lpY, 15+((bass/255)*10));
 
-   
- 
-    //Light 2 // lgiht is very diuffuse// not enought treble in the piece. 
+
+
+    //Light 2 // lgiht is very diuffuse// not enought treble in the piece.
     if(lpX2 > width/2  || lpY2 > height/2 || lpX2 <= -width/2 || lpY2 <= -height/2){
       lpX2 = random(width/2);
       lpY2 = random(height/2);
@@ -210,8 +213,8 @@ function light_4(lFreq){ // crawler again with new lpX -- need to fix theese and
     lpX2  = lpX2 + random(-4,2);
     lpY2  = lpY2 + random(-2,3);
     let pl2= pointLight((treble+highMid+mid)/3, (treble+highMid+mid)/3, (treble+highMid+mid)/3, lpX2, lpY2, 10+((treble+highMid+mid)/765)*10);
-  
-  
+
+
   // Drunk Walker
   if(lpX3 > width/2  || lpY3 > height/2 || lpX3 <= -width/2 || lpY3 <= -height/2){
       lpX3 = random(width/2);
@@ -222,4 +225,3 @@ function light_4(lFreq){ // crawler again with new lpX -- need to fix theese and
     let pl3= pointLight((lowMid+mid+highMid)/3, (lowMid+mid+highMid)/3, (lowMid+mid+highMid)/3, lpX3, lpY3, 5+((lowMid+mid+highMid)/765)*10);
     cnt += .01;
     */
-
