24f318fc89815002affb73be9bfd1c4e44c3c879
Stephen Roddy <roddyst@tcd.ie>
2022-01-11 13:35:26 +0000

    edits

--- scripts/loadUp.js
+++ scripts/loadUp.js
@@ -1,10 +1,10 @@
 var myVar;
 
-function myFunction() {
+function loadUp() {
   myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("displayer").style.display = "block";
 }
